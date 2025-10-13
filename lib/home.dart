2db2f3ee0faa84dc1613593a89c0c675922336db
JehuelTP <jehueltorrico@gmail.com>
2025-10-13 11:16:08 -0400

    Widgets Stack, Column, Row, Container, Text y Icon/Metodo constructor de un Widget/Typografias de Google Fonts

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'description_place.dart';
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final descriptionPlace = Container(
+      margin: EdgeInsets.only(
+        top: 250,
+        left: 25,
+        right: 25
+      ),
+      child: DescriptionPlace("Uyuni", 4, "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original."),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Places",
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          descriptionPlace
+        ],
+      )
+    );
+  }
+}
